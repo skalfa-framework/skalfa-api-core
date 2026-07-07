40b57631e8924cdae38c9728e1d3a16c30865827
DarkLocuts <darklocuts23@gmail.com>
2026-07-07 13:12:33 +0700

    feat: implement getUserPermissions helper to aggregate user and role-based permissions

--- src/auth/helpers/get-user-permissions.ts
+++ src/auth/helpers/get-user-permissions.ts
@@ -1,7 +1,7 @@
 import { db } from '@skalfa/skalfa-orm'
 
 export async function getUserPermissions(userId: number): Promise<string[]> {
-  const roleIds = await db("user_roles").where("user_id", userId).pluck("role_id")
+  const roleIds = await db("user_roles").where("user_id", userId).pluck("user_role_id")
 
   const query = db("user_permissions").where("user_id", userId)
 
