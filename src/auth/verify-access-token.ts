539203eaf0b9e7c83977b19af0e561bc4d346d36
DarkLocuts <darklocuts23@gmail.com>
2026-07-17 15:24:53 +0700

    feat: implement verifyAccessToken utility for secure token validation and caching and bump package version to 1.0.27

--- src/auth/verify-access-token.ts
+++ src/auth/verify-access-token.ts
@@ -35,30 +35,7 @@ export async function verifyAccessToken(token: string, req?: Request) {
 
   await db("user_access_tokens").where("id", tokenRecord.id).update({ last_used_at: new Date(), last_used_ip: ip })
 
-  const rows = await db("users")
-    .leftJoin("user_has_user_roles", "users.id", "user_has_user_roles.user_id")
-    .leftJoin("user_roles", "user_has_user_roles.user_role_id", "user_roles.id")
-    .where("users.id", tokenRecord.user_id)
-    .select(
-      "users.id",
-      "users.username",
-      "users.name",
-      "user_roles.id as role_id",
-      "user_roles.name as role_name"
-    )
-
-  if (!rows || rows.length === 0) return null
-
-  const firstRow = rows[0]
-  const user = {
-    id        :  firstRow.id,
-    username  :  firstRow.username,
-    name      :  firstRow.name,
-    roles     :  rows?.filter((row: any) => row && row.role_id != null)?.map((row: any) => ({
-      id    :  row.role_id,
-      name  :  row.role_name
-    })) || [],
-  }
+  const user = await db("users").where("id", tokenRecord.user_id).first()
 
   if (AUTH_CACHE) {
     const redis = registry.get('redis')
